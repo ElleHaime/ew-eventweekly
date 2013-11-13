3214eb016d53301c72215aa9e7c790cd268db99d
Jake Badland <jake@tiestegroup.com>
2013-11-13 15:09:49 +0200

    'merged'

--- public/js/interface.js
+++ public/js/interface.js
@@ -2,31 +2,21 @@ $( document ).ready(function() {
     $( "#event-join" ).click(function() {
         $('.categ_yellow').hide();
         $('.categ_green').show();
+        $.post("/event/answer", { answer: 'JOIN', event_id : $('#event_id').val() });
         FB.ui({
             method: 'feed',
             link:   window.location.href,
             caption: 'You are joined event'
-        }, function(response){
-            if (response != null)
-            {
-                $.post("answer", { uid: response.authResponse.userID, access_token: access_token },
-                    function(data) {
-                        data = jQuery.parseJSON(data);
-                        if (data.status=='OK')
-                        {
-                            console.log('saved');
-                        }
-                    });
-            }
-        });
-
+        }, function(response){});
     });
     $( "#event-maybe" ).click(function() {
         $('.categ_green').hide();
         $('.categ_yellow').show();
+        $.post("/event/answer", { answer: 'MAYBE', event_id : $('#event_id').val() });
     });
     $( "#event-decline" ).click(function() {
         $('.categ_green').hide();
         $('.categ_yellow').hide();
+        $.post("/event/answer", { answer: 'DECLINE', event_id : $('#event_id').val() });
     });
 });
