57a5c1dfce05d8ea55b7654d6dc1a5e0a716eeba
Stas <devnull@localhost>
2014-01-31 14:06:23 +0200

    Fixed liked counter

--- apps/frontend/models/EventLike.php
+++ apps/frontend/models/EventLike.php
@@ -9,12 +9,10 @@ class EventLike extends EventLikeObject
     public function getLikedEventsCount($uId)
     {
         if ($uId) {
-            return self::find(array('member_id = ' . $uId . " AND status = 1"))->count();
-
-            /*$event = new Event();
-            $event->addCondition('Objects\EventLike.member_id = ' . $uId);
-            $event->addCondition('Objects\EventLike.status = 1');
-            $event->addCondition('Frontend\Models\Event.event_status = 1');*/
+            $event = new Event();
+            $event->addCondition('Frontend\Models\EventLike.member_id = ' . $uId);
+            $event->addCondition('Frontend\Models\EventLike.status = 1');
+            $event->addCondition('Frontend\Models\Event.event_status = 1');
 
             return $event->fetchEvents()->count();
         } else {
