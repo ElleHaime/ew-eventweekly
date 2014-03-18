5fefaa07606683c44afb0283c6a92c9e53e727e4
sv <svrazina@tiestegroup.com>
2014-03-18 12:57:57 +0200

    cache on logout

--- apps/frontend/controllers/AuthController.php
+++ apps/frontend/controllers/AuthController.php
@@ -321,11 +321,11 @@ class AuthController extends \Core\Controller
      */
     public function logoutAction()
     {
-        $keys = $this -> cacheData -> queryKeys();
+/*        $keys = $this -> cacheData -> queryKeys();
         foreach ($keys as $key) {
             $this -> cacheData -> delete($key);
         }
-        
+*/        
 		$this -> session -> destroy();
 		return $this -> response -> redirect('/');
     }
