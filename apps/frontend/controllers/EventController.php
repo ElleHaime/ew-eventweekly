2585cb08f79e0c5879948fa527c653f6a1a78519
sv <svrazina@tiestegroup.com>
2014-05-05 18:03:20 +0300

    fixed images

--- apps/frontend/controllers/EventController.php
+++ apps/frontend/controllers/EventController.php
@@ -1137,10 +1137,10 @@ class EventController extends \Core\Controllers\CrudController
             $Event->addCondition('Frontend\Models\Event.latitude BETWEEN ' . $loc->latitudeMin . ' AND ' . $loc->latitudeMax . '
         						AND Frontend\Models\Event.longitude BETWEEN ' . $loc->longitudeMin . ' AND ' . $loc->longitudeMax . '
         						AND Frontend\Models\Event.start_date > "' . date('Y-m-d H:i:s', strtotime('today -1 minute')) . '"' . '
-        						AND Frontend\Models\Event.start_date < "' . date('Y-m-d H:i:s', strtotime('today +20 days')) . '"');
+        						AND Frontend\Models\Event.start_date < "' . date('Y-m-d H:i:s', strtotime('today +3 days')) . '"');
         } else {
             $Event->addCondition('Frontend\Models\Event.start_date > "' . date('Y-m-d H:i:s', strtotime('today -1 minute')) . '"');
-            $Event->addCondition('Frontend\Models\Event.start_date < "' . date('Y-m-d H:i:s', strtotime('today +20 days')) . '"');
+            $Event->addCondition('Frontend\Models\Event.start_date < "' . date('Y-m-d H:i:s', strtotime('today +3 days')) . '"');
         }
         $Event->addCondition('Frontend\Models\Event.id > ' . $this->session->get('lastFetchedEvent'));
         $Event->addCondition('Frontend\Models\Event.event_status = 1');
