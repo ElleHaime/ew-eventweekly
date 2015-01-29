2ab70114bc15f2a41b54851a15bbb640a6274ce2
serge <likeserge@gmail.com>
2015-01-29 18:22:53 +0200

    done with search list results

--- public/js/frontend/general/lazyLoader.js
+++ public/js/frontend/general/lazyLoader.js
@@ -7,15 +7,32 @@ define('lazyLoader',
         	var self = this;
 
         	self.settings = 
-        	{
-        		loadMoreButton: '#load_more',
-        		divToUpdate: '.page__wrapper',
-        		divToOverlay: '.page'
-        	},
+            {
+                loadMoreButton: '#load_more',
+                divToUpdate: '.page__wrapper',
+                divToOverlay: '.page',
+                preloader: '#preloader',
+                pageNumber: 1,
+                url: window.location.href,
+                nextPageUrl: '',
+                totalPagesJs: 0,
+
+
+                
+            },
 
         	self.init = function()
         	{
-        		alert('bububu');
+        		$(self.settings.loadMoreButton).hide();
+                self.settings.totalPagesJs = document.getElementById("totalPagesJs").textContent;
+        
+    
+                if ( self.settings.totalPagesJs>1 ) {
+                    $(self.settings.loadMoreButton).show();
+                }       
+
+                $("img.lazy").lazyload();
+
         		// initialize clicks
         		self.__bindClicks();
         	},
@@ -23,15 +40,49 @@ define('lazyLoader',
         	self.__bindClicks = function() 
         	{
         		$(self.settings.loadMoreButton).click(function() {
-        			alert('ololo');
+        			//alert('ololo');
         			self.__someFunc();
         		});
-        		alert('trampampam');
+        		//alert('trampampam');
         	},
 
         	self.__someFunc = function()
         	{
-        		alert('someFunc');
+        		//alert('someFunc');
+                 $("img.lazy").lazyload();
+        
+                //overlay before request done
+                $(self.settings.divToOverlay)
+                    .css({
+                        'opacity' : 0.4,
+                        'background-color': 'black',
+                    });
+                $(self.settings.preloader)
+                    .css({
+                        'position' : 'fixed',
+                        'top': '30%',
+                        'left': $(window).width()/2-128/2,
+                        'display': 'inline'
+                    });
+
+
+                self.settings.pageNumber++;
+
+                if ( self.settings.pageNumber>=self.settings.totalPagesJs ) {
+                    $(self.settings.loadMoreButton).hide();
+                }
+
+
+                
+                nextPageUrl = self.settings.url + '&page=' + self.settings.pageNumber;
+                $.ajax({url:nextPageUrl,success:function(result){
+                    $(self.settings.divToUpdate).html(result);
+                    $(self.settings.divToOverlay)
+                        .css({
+                            'opacity' : 1,
+                            'background-color': 'transparent',
+                        });
+                }});
         	}
         };
         
