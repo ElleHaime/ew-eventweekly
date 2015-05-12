cac167162eb247bc8b5fd5cb088f3c57a8b32a09
sv <devnull@localhost>
2015-05-12 15:04:37 +0300

    fix trending list

---
+++ public/js/frontend/event/trending.js
@@ -0,0 +1,22 @@
+require([
+	'jquery',
+	'fb',
+	'frontEventLike',
+	'noty',
+	'lazyLoader',
+	'utils',
+	'domReady',
+	'underscore',
+	'jCookie'
+	], 
+	function($, fb, frontEventLike, noty, lazyLoader) {
+		fb.init(); 
+		frontEventLike.init();
+		lazyLoader.init();
+		
+		if ($('#splash_messages').length > 0) {
+			var fMessage = $('#splash_messages');
+            noty({text: fMessage.attr('flashMsgText'), type: fMessage.attr('flashMsgType')});
+		}
+	}
+);
